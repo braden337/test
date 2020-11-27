917bfd9ebe28be6f1f7a86a685183372fc53b5ca
Braden Jonsson <braden337@gmail.com>
2020-11-27 15:37:41 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+import moment from "moment";
+
+let time = document.querySelector("time");
+
+let milliseconds = Number(time.dataset.milliseconds);
+
+let m = moment(milliseconds);
+
+time.textContent = m.fromNow();
